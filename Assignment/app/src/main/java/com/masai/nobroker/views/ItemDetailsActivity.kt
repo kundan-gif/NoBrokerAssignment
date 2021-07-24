24b87d3a4ea613705936830401e300668249824b
Kundan Kumar <kundan211097@gmail.com>
2021-07-25 01:35:50 +0530

    Splash activity and backstack implemented

--- Assignment/app/src/main/java/com/masai/nobroker/views/ItemDetailsActivity.kt
+++ Assignment/app/src/main/java/com/masai/nobroker/views/ItemDetailsActivity.kt
@@ -1,7 +1,11 @@
 package com.masai.nobroker.views
 
+import android.annotation.SuppressLint
 import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.masai.nobroker.R
 import kotlinx.android.synthetic.main.activity_item_details.*
@@ -11,12 +15,36 @@ class ItemDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item_details)
+        setupActionBar()
+        statuscolor()
         if(intent.extras!=null){
             val byteArray = intent.getByteArrayExtra("imageKey")
             val bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray!!.size)
             ivImage.setImageBitmap(bmp)
             tvTitle.text=intent.getStringExtra("titleKey")
             tvSubTitle.text=intent.getStringExtra("subTitleKey")
+        }
+    }
+    /**
+     * A function for actionBar Setup.
+     */
+    private fun setupActionBar() {
+        setSupportActionBar(toolbar_item_details_activity)
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24)
+        }
+
+        toolbar_item_details_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+    fun statuscolor() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            val window = this.window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.statusBarColor = this.resources.getColor(R.color.colorPrimaryDark)
+            getWindow().decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
         }
     }
 }
---
+++ Assignment/app/src/main/java/com/masai/nobroker/views/SplashActivity.kt
@@ -0,0 +1,48 @@
+package com.masai.nobroker.views
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import com.masai.nobroker.R
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        val splashAnimation = AnimationUtils.loadAnimation(this@SplashActivity, R.anim.anim_splash)
+        tv_app_name.animation = splashAnimation
+        splashAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+
+            }
+            override fun onAnimationEnd(animation: Animation?) {
+                Handler(Looper.getMainLooper()).postDelayed({
+                    startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                    finish()
+                }, 3000)
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+
+            }
+        })
+    }
+}
